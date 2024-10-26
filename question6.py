8318b34f0ac28e95b64e077f7e391ef9a4b540be
DevEcho625 <anuj.m.bhatia@gmail.com>
2024-10-26 19:09:46 +0530

    Create question6.py

---
+++ question6.py
@@ -0,0 +1,8 @@
+numberOfNumbers = 100
+sum_of_squares = 0
+for x in range(1,numberOfNumber+1):
+  sum_of_sqaures = x**2
+
+squaree_of_sum = 0
+square_of_sum = ((numberOfNumbers*numberOfNumber+1)/2)**2
+print(sum_of_squares - square_of_sum)
