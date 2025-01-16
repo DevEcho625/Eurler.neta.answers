32482ce174a62ec817505e90a3ddaa9d28baa6e6
DevEcho625 <anuj.m.bhatia@gmail.com>
2025-01-16 15:07:35 +0530

    Update question12.py

--- question12.py
+++ question12.py
@@ -7,15 +7,14 @@ def factorCount(number):
             factorCount = factorCount + 1
     return 2*factorCount
 
-print(factorCount(4))
 triangleNumbers = []
 for x in range (1, 50000):
     triangleNumbers.append(x*(x+1)*0.5)
-print(1)
 
 for x in triangleNumbers:
     if factorCount(x) >= 500:
         print(x)
+        break
 
-print(factorCount(triangleNumbers[len(triangleNumbers)-1]))
-print("HI")
+
+
